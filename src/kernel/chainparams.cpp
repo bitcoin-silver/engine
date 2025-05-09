c1f7255a4611c96bc15ec7372a077d9a6bcfe316
MrVistos <m.bambenek@protonmail.com>
2025-05-09 17:21:33 +0200

    Fix ports

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -312,7 +312,7 @@ public:
         pchMessageStart[1] = 0xbf;
         pchMessageStart[2] = 0xb5;
         pchMessageStart[3] = 0xfe;
-        nDefaultPort = 110566;
+        nDefaultPort = 10666;
         nPruneAfterHeight = 1000;
         m_assumed_blockchain_size = 1;
         m_assumed_chain_state_size = 1;
@@ -556,7 +556,7 @@ public:
         uint256 hash = h.GetHash();
         std::copy_n(hash.begin(), 4, pchMessageStart.begin());
 
-        nDefaultPort = 310566;
+        nDefaultPort = 10766;
         nPruneAfterHeight = 1000;
 
         genesis = CreateTestnetGenesisBlock(1598918400, 52613770, 0x1e0377ae, 1, 50 * COIN);
